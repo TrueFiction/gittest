ab3759a46294dff460050f675efde45ba56c9c9f
Ryan Milkovits <rmilkovi@ucsd.edu>
2016-04-17 10:56:38 -0700

    updated test files to pass now

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -17,7 +17,7 @@ public class Celsius extends Temperature {
         public String toString()
         {
             // TODO: Complete this method
-            return ""/* + this.getValue() + " C"*/;
+            return ""+ this.getValue() + " C";
         }
 
     @Override
